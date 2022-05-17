0d9b57cad793c9e06a4156210edc1216491c152f
kalyan ds <96898213+kalyands@users.noreply.github.com>
2022-05-17 16:27:39 +0000

    assignment 1 p5

--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -1,12 +1,16 @@
 # Functions
 
 
-def computepay(h, r):
-    pass  # ...
-
-
-hrs = float(input("Enter hours? "))
-rte = float(input("Enter rate per hour? "))
-
-p = computepay(hrs, rte)
-print("Pay", p)
+score = float(input("Enter Score: "))
+if (score >= 0.9 and score < 1.0 and score > 0.0):
+    print("A")
+elif (score >= 0.8 and < 1.0 and score > 0.0):
+    print("B")
+elif (score >= 0.7 and < 1.0 and score > 0.0):
+    print("C")
+elif (score >= 0.6 and < 1.0 and score > 0.0):
+    print("D")
+elif (score < 0.6 and < 1.0 and score > 0.0):
+    print("F")
+elif (score > 1.0 and score < 0.0):
+    print("error")
