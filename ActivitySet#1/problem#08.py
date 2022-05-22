fc9fe86880779d6a462ad3e39072dbb2acc090ba
kalyan ds <96898213+kalyands@users.noreply.github.com>
2022-05-22 13:03:39 +0000

    assignment 1 p8

--- ActivitySet#1/problem#08.py
+++ ActivitySet#1/problem#08.py
@@ -1,3 +1,21 @@
-# Files
-
-filename = "dataset/mbox-short.txt"
+ fh = open('mbox-short.txt')
+for line in fh:
+    if not line.startswith("X-DSPAM-Confidence: 0.8475"):
+        pos = str.find('5')
+        print (pos)
+        continue
+        
+else:
+    with open('mbox-short.txt') as fh:
+    x = len(fp.readlines())
+    print('Total lines:', x)
+    value = float(x)
+    print(value)
+    
+    line in lines:
+    for c in line:
+        if c.isdigit() == True:
+            sum = sum + int(c)
+            print (sum)
+   
+print("Done")
