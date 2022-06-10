fadd876ab530ee58b6a6657ecc9d008e64500a5e
kalyan ds <96898213+kalyands@users.noreply.github.com>
2022-06-10 15:29:09 +0000

    function example 1

---
+++ ActivitySet#1/functionexample.py
@@ -0,0 +1,8 @@
+def iseven(x):
+    if x%2 == 0:
+          return true
+          else
+        
+              return false
+              l=[0,5,10,15,20,60]
+              l1 = list(fliter (is even,I) )
