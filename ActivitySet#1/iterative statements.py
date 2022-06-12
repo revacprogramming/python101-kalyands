71c691e4250e33f8dee232be17959b9f4dc222fd
kalyan ds <96898213+kalyands@users.noreply.github.com>
2022-06-12 06:40:50 +0000

    iterative statements ex2

--- ActivitySet#1/iterative statements.py
+++ ActivitySet#1/iterative statements.py
@@ -1,6 +1,7 @@
 #iterative statements
-s = input("enter the string")
-i=0
-for x in s:
-    print("the character present at ",i,"index is ",x)
-    i=i+1
+list=eval(input("enter list"))
+sum=0;
+for x in list:
+    sum=sum+x;
+    print("the sum=",sum)
+    
