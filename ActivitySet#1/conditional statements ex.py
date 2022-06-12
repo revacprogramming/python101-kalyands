4fafa4a03e2527d506765f909494188ee0f40479
kalyan ds <96898213+kalyands@users.noreply.github.com>
2022-06-12 06:24:13 +0000

    conditional statements ex 2

---
+++ ActivitySet#1/conditional statements ex.py
@@ -0,0 +1,11 @@
+# conditional statements
+
+n1 = int(input("enter the number n1"))
+n2 = int(input("enter the number n2"))
+n3 = int(input("enter the number n3"))
+if n1>n2 and n1>n3:
+    print("the biggest number is",n1)
+    elif n2>n3:
+         print("the biggest number is ",n2)
+         else:
+              print("the biggest number is",n3)
